3d6dd1bcb14db2ad3e74dcc438f3ce219efb7f69
Darbaz Ali <darbaz.me@gmail.com>
2020-11-23 10:34:37 +0300

    Header, Footer, React-bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,23 @@
+import { BrowserRouter } from 'react-router-dom'
+import { Route, Switch } from 'react-router'
+
+import { Container } from 'react-bootstrap'
+
+import HomePage from './screens/HomePage.js'
+import Header from './components/Header.js'
+import Footer from './components/Footer.js'
+
 function App() {
   return (
-    <div className='App'>
-      <h1>Issue Tracker | Home</h1>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Container>
+        <Switch>
+          <Route path='/' exact component={HomePage} />
+        </Switch>
+      </Container>
+      <Footer />
+    </BrowserRouter>
   )
 }
 
---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+
+const Footer = () => {
+  return (
+    <footer>
+      <Container>
+        <p>© 2020 - Issue Tracker by Darbaz Ali</p>
+      </Container>
+    </footer>
+  )
+}
+
+export default Footer
